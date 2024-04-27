1850fde2f179caa8deca56c11c7ac57965811110
Minayaff <minayaff@code.edu.az>
2024-04-27 08:09:45 +0400

    one to many

--- DMAWebProject/Models/Products.cs
+++ DMAWebProject/Models/Products.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DMAWebProject.Models
 {
@@ -10,6 +11,7 @@ namespace DMAWebProject.Models
         public double Price { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
     }
 }
